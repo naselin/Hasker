6d70b2b437132edb7e116cfb0c63ccecaff40a7c
Nikita A Selin <nikita.selin@gmail.com>
2020-01-06 17:35:38 +0300

    Configuration fixes.

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,9 +1,24 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 ALLOWED_HOSTS = []
+
+SECRET_KEY = '^2%sixf#^6+(%f(^sm9p9b@er82)!2kh$eb7n-w#a-%qm-%-t2'
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hasker',
+        'USER': 'hasker',
+        'PASSWORD': 'h@$keRApp1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,11 +2,27 @@ from .base import *
 
 DEBUG = False
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 ALLOWED_HOSTS = ['*']
 
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+# Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/haskermail'
 
+# Pagination limits
 QUESTIONS_PER_PAGE = 20
 ANSWERS_PER_PAGE = 30
 TRENDING_QUESTIONS = 20
